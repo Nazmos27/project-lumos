08df5f083a46f758cf473e7b2b558cb814ea438b
Nazmos27 <nazmos789@gmail.com>
2023-10-26 19:30:05 +0600

    semester_12 data entry

--- routineData.js
+++ routineData.js
@@ -1,6 +1,102 @@
 export default routineData = [
     {
         id:1,
+        session: "2019-20",
+        semester: "32",
+        course:[
+            {
+                code: "3201",
+                name:"Intelectual Property Law",
+                instructor : "Mahmuda Amir Eva",
+                info: [
+                    {
+                        day:"monday",
+                        room: "916",
+                        time: "1.30pm - 2.20pm"
+                    },
+                    {
+                        day: "tuesday",
+                        room: "917",
+                        time: "11.15am - 12.05pm"
+                    },
+                    {
+                        day: "thursday",
+                        room: "915",
+                        time: "11.15am - 1.00pm"
+                    }
+                ]
+            },
+            {
+                code: "3202",
+                name:"Penal Code",
+                instructor : "Mahmudul Hasan Joy",
+                info: [
+                    {
+                        day:"monday",
+                        room: "910",
+                        time: "8.30am - 9.20am"
+                    },
+                    {
+                        day:"tuesday",
+                        room: "916",
+                        time: "8.30am - 9.20am"
+                    },
+                    {
+                        day: "wednesday",
+                        room: "917",
+                        time: "1.30pm - 3.15pm"
+                    }
+                ]
+            },
+            {
+                code: "3203",
+                name:"Equity,Law Of Trusts & Public Demands Recovery",
+                instructor : "Ms. Munira Jahan Sumi",
+                info: [
+                    {
+                        day:"monday",
+                        room: "915",
+                        time: "9.25am - 11.10am"
+                    },
+                    {
+                        day: "wednesday",
+                        room: "916",
+                        time: "11.15am - 12.05pm"
+                    },
+                    {
+                        day: "no",
+                        room: "no",
+                        time: "no"
+                    }
+                ]
+            },
+            {
+                code: "3204",
+                name:"Human Rights",
+                instructor : "Mr. Khair Mahmud",
+                info: [
+                    {
+                        day:"sunday",
+                        room: "915",
+                        time: "11.15am - 1.00pm"
+                    },
+                    {
+                        day: "tuesday",
+                        room: "917",
+                        time: "12.10pm - 1.00pm"
+                    },
+                    {
+                        day: "wednesday",
+                        room: "916",
+                        time: "11.15am - 12.05pm"
+                    }
+                ]
+            }
+        ]
+    },
+
+    {
+        id:2,
         session: "2020-21",
         semester: "22",
         course:[
@@ -94,100 +190,6 @@ export default routineData = [
             }
         ]
     },
-    {
-        id:2,
-        session: "2019-20",
-        semester: "32",
-        course:[
-            {
-                code: "3201",
-                name:"Intelectual Property Law",
-                instructor : "Mahmuda Amir Eva",
-                info: [
-                    {
-                        day:"monday",
-                        room: "916",
-                        time: "1.30pm - 2.20pm"
-                    },
-                    {
-                        day: "tuesday",
-                        room: "917",
-                        time: "11.15am - 12.05pm"
-                    },
-                    {
-                        day: "thursday",
-                        room: "915",
-                        time: "11.15am - 1.00pm"
-                    }
-                ]
-            },
-            {
-                code: "3202",
-                name:"Penal Code",
-                instructor : "Mahmudul Hasan Joy",
-                info: [
-                    {
-                        day:"monday",
-                        room: "910",
-                        time: "8.30am - 9.20am"
-                    },
-                    {
-                        day:"tuesday",
-                        room: "916",
-                        time: "8.30am - 9.20am"
-                    },
-                    {
-                        day: "wednesday",
-                        room: "917",
-                        time: "1.30pm - 3.15pm"
-                    }
-                ]
-            },
-            {
-                code: "3203",
-                name:"Equity,Law Of Trusts & Public Demands Recovery",
-                instructor : "Ms. Munira Jahan Sumi",
-                info: [
-                    {
-                        day:"monday",
-                        room: "915",
-                        time: "9.25am - 11.10am"
-                    },
-                    {
-                        day: "wednesday",
-                        room: "916",
-                        time: "11.15am - 12.05pm"
-                    },
-                    {
-                        day: "no",
-                        room: "no",
-                        time: "no"
-                    }
-                ]
-            },
-            {
-                code: "3204",
-                name:"Human Rights",
-                instructor : "Mr. Khair Mahmud",
-                info: [
-                    {
-                        day:"sunday",
-                        room: "915",
-                        time: "11.15am - 1.00pm"
-                    },
-                    {
-                        day: "tuesday",
-                        room: "917",
-                        time: "12.10pm - 1.00pm"
-                    },
-                    {
-                        day: "wednesday",
-                        room: "916",
-                        time: "11.15am - 12.05pm"
-                    }
-                ]
-            }
-        ]
-    }
+    
 
 ]
